9c64e508e1e705a1eea71f3cfc0711e4e803df99
Roman Geints <romang@guarda.co>
2021-12-20 20:03:35 +0300

    Bring back ASM.JS

--- monero_utils/MyMoneroCoreBridge.js
+++ monero_utils/MyMoneroCoreBridge.js
@@ -1,3 +1,4 @@
+const loadScriptInBrowser = require('load-script')
 const MyMoneroCoreBridgeClass = require('./MyMoneroCoreBridgeClass')
 const MyMoneroBridge_utils = require('@mymonero/mymonero-bridge-utils')
 //
@@ -12,15 +13,70 @@ module.exports = function(options)
 	const ENVIRONMENT_IS_WORKER = platform_info.ENVIRONMENT_IS_WORKER;
 	const ENVIRONMENT_IS_NODE = platform_info.ENVIRONMENT_IS_NODE;
 	const ENVIRONMENT_IS_SHELL = platform_info.ENVIRONMENT_IS_SHELL;
-	//
-	function locateFile(filename, scriptDirectory)
+
+	function getDefaultScriptDirectory() {
+		let scriptDirectory = '';
+
+		if (ENVIRONMENT_IS_NODE) {
+			if (ENVIRONMENT_IS_WORKER) {
+				scriptDirectory = require("path").dirname(scriptDirectory) + "/"
+			} else {
+				scriptDirectory = __dirname + "/"
+			}
+		} else if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_WORKER) {
+			if (ENVIRONMENT_IS_WORKER) {
+				scriptDirectory = self.location.href
+			} else if (typeof document !== "undefined" && document.currentScript) {
+				scriptDirectory = document.currentScript.src
+			}
+
+			if (scriptDirectory.indexOf("blob:") !== 0) {
+				scriptDirectory = scriptDirectory.substr(0, scriptDirectory.replace(/[?#].*/, "").lastIndexOf("/") + 1);
+			} else {
+				scriptDirectory = "";
+			}
+		}
+
+		return scriptDirectory;
+	}
+
+	function loadScript(fileLocation) {
+		if (ENVIRONMENT_IS_NODE) {
+			let effectiveRequire = require;
+
+			if (typeof __non_webpack_require__ !== 'undefined') {
+				effectiveRequire = __non_webpack_require__;
+			}
+
+			return Promise.resolve(effectiveRequire(fileLocation));
+		} else if (ENVIRONMENT_IS_WEB) {
+			if (typeof MyMoneroCoreCpp !== 'undefined') {
+				return Promise.resolve(MyMoneroCoreCpp);
+			}
+
+			return new Promise(function (resolve, reject) {
+				loadScriptInBrowser(fileLocation, function (error) {
+					if (error) {
+						reject(error);
+						return;
+					}
+
+					resolve(MyMoneroCoreCpp);
+				});
+			});
+		}
+
+		throw new Error('Not sure how to load: ' + fileLocation);
+	}
+
+	function locateFile(filename, scriptDirectory = getDefaultScriptDirectory())
 	{
 		// if (options["locateFile"]) {
-		// 	return options["locateFile"](filename, scriptDirectory)
+		//		return options["locateFile"](filename, scriptDirectory)
 		// }
 		var this_scriptDirectory = scriptDirectory
 		const lastChar = this_scriptDirectory.charAt(this_scriptDirectory.length - 1)
-		if (lastChar == "/" || lastChar == "\\") { 
+		if (lastChar == "/" || lastChar == "\\") {
 			// ^-- this is not a '\\' on Windows because emscripten actually appends a '/'
 			this_scriptDirectory = this_scriptDirectory.substring(0, this_scriptDirectory.length - 1) // remove trailing "/"
 		}
@@ -43,7 +99,10 @@ module.exports = function(options)
 				// have to check != "/" b/c webpack (I think) replaces __dirname
 				pathTo_cryptonoteUtilsDir = "file://" + __dirname + "/" // prepending "file://" because it's going to try to stream it
 			} else { // actual web browser
-				pathTo_cryptonoteUtilsDir = this_scriptDirectory + `/mymonero_core_js/monero_utils/` // this works for the MyMonero browser build, and is quite general, at least
+				pathTo_cryptonoteUtilsDir = this_scriptDirectory + '/'
+				if (pathTo_cryptonoteUtilsDir.indexOf('/mymonero_core_js/monero_utils/') === -1) {
+					pathTo_cryptonoteUtilsDir = pathTo_cryptonoteUtilsDir + 'mymonero_core_js/monero_utils/' // this works for the MyMonero browser build, and is quite general, at least
+				}
 			}
 			fullPath = pathTo_cryptonoteUtilsDir + filename
 		}
@@ -53,18 +112,30 @@ module.exports = function(options)
 		//
 		return fullPath
 	}
-	return new Promise(function(resolve, reject) {
-		var Module_template = {}
-		
-		Module_template["locateFile"] = locateFile
-		
-		require(`./MyMoneroCoreCpp_WASM`)(Module_template).then(function(thisModule) 
-		{
-			const instance = new MyMoneroCoreBridgeClass(thisModule);
-			resolve(instance);
-		}).catch(function(e) {
-			console.error("Error loading WASM_MyMoneroCoreCpp:", e);
-			reject(e);
+
+	return Promise.resolve({ locateFile })
+		.then(function (Module_template) {
+			return require(`./MyMoneroCoreCpp_WASM`)(Module_template);
+		})
+		.then(function(thisModule) {
+			return new MyMoneroCoreBridgeClass(thisModule);
+		})
+		.catch(function(error) {
+			console.error("Error loading MyMoneroCoreCpp_WASM:", error);
+			return Promise.resolve({ locateFile })
+				.then(function (Module_template) {
+					return loadScript(locateFile('MyMoneroCoreCpp_ASMJS.js'))
+						.then(function (MyMoneroCoreCpp_ASMJS) {
+							return MyMoneroCoreCpp_ASMJS(Module_template);
+						});
+				})
+				.then(function(thisModule) {
+					return new MyMoneroCoreBridgeClass(thisModule);
+				})
+				.catch(function(error2) {
+					console.error("Error loading MyMoneroCoreCpp_ASMJS:", error2);
+					error2.relatedError = error2;
+					throw error2;
+				});
 		});
-	});
 };
